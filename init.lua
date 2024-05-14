e68fd951342be9f59289f861c19a5805d06b9257
Jussi Heikkilä <jussih@gmail.com>
2024-05-14 18:19:01 +0300

    ruff_lsp, ruff_format, cmp-nvim-lsp-signature-help

--- init.lua
+++ init.lua
@@ -564,6 +564,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        ruff_lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -650,7 +651,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { 'isort', 'black' },
-        python = { { 'ruff', 'black' } },
+        python = { { 'ruff_format', 'black' } },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
@@ -694,6 +695,7 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
     },
     config = function()
       -- See `:help cmp`
@@ -763,6 +765,7 @@ require('lazy').setup({
         },
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'nvim_lsp_signature_help' },
           { name = 'luasnip' },
           { name = 'path' },
         },
